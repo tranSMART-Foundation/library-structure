d0b0aef0fba1c3fced5f36cb0db61982473bf9c7
The transmart base account <transmart@ip-172-31-13-213.ec2.internal>
2016-04-05 19:16:01 +0000

    initial content structure from library

---
+++ datasets/fetch.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+cd /var/www/datasets
+
+#mkdir /var/www/datasets/Cell-line
+#cd /var/www/datasets/Cell-line
+#curl http://studies.thehyve.net/Cell-line/Cell-line_acgh.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_clinical.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_expression.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_mirna.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_proteomics.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_ref_annotation.tar.xz -O
+#curl http://studies.thehyve.net/Cell-line/Cell-line_vcf.tar.xz -O
+
+#mkdir /var/www/datasets/GPL1291
+#cd /var/www/datasets/GPL1291
+#curl http://studies.thehyve.net/GPL1291/GPL1291_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL14663
+#cd /var/www/datasets/GPL14663
+#curl http://studies.thehyve.net/GPL14663/GPL14663_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL4133
+#cd /var/www/datasets/GPL4133
+#curl http://studies.thehyve.net/GPL4133/GPL4133_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL570
+#cd /var/www/datasets/GPL570
+#curl http://studies.thehyve.net/GPL570/GPL570_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL6480
+#cd /var/www/datasets/GPL6480
+#curl http://studies.thehyve.net/GPL6480/GPL6480_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL7264
+#cd /var/www/datasets/GPL7264
+#curl http://studies.thehyve.net/GPL7264/GPL7264_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL7546
+#cd /var/www/datasets/GPL7546
+#curl http://studies.thehyve.net/GPL7546/GPL7546_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GPL96
+#cd /var/www/datasets/GPL96
+#curl http://studies.thehyve.net/GPL96/GPL96_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GSE13168
+#cd /var/www/datasets/GSE13168
+#curl http://studies.thehyve.net/GSE13168/GSE13168_clinical.tar.xz -O
+#curl http://studies.thehyve.net/GSE13168/GSE13168_expression.tar.xz -O
+#curl http://studies.thehyve.net/GSE13168/GSE13168_ref_annotation.tar.xz -O
+
+#mkdir /var/www/datasets/GSE13732
+#cd /var/www/datasets/GSE13732
+#curl http://studies.thehyve.net/GSE13732/GSE13732_clinical.tar.xz -O
+#curl http://studies.thehyve.net/GSE13732/GSE13732_expression.tar.xz -O
+#curl http://studies.thehyve.net/GSE13732/GSE13732_ref_annotation.tar.xz -O
+
+
+mkdir /var/www/datasets/GSE1456
+cd /var/www/datasets/GSE1456
+curl http://studies.thehyve.net/GSE1456/GSE1456_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE1456/GSE1456_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE1456/GSE1456_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE15258
+cd /var/www/datasets/GSE15258
+curl http://studies.thehyve.net/GSE15258/GSE15258_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE15258/GSE15258_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE15258/GSE15258_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE17755
+cd /var/www/datasets/GSE17755
+curl http://studies.thehyve.net/GSE17755/GSE17755_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE17755/GSE17755_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE17755/GSE17755_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE20194
+cd /var/www/datasets/GSE20194
+curl http://studies.thehyve.net/GSE20194/GSE20194_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE20194/GSE20194_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE20194/GSE20194_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE20690
+cd /var/www/datasets/GSE20690
+curl http://studies.thehyve.net/GSE20690/GSE20690_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE20690/GSE20690_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE20690/GSE20690_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE24060
+cd /var/www/datasets/GSE24060
+curl http://studies.thehyve.net/GSE24060/GSE24060_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE24060/GSE24060_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE24060/GSE24060_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE27831
+cd /var/www/datasets/GSE27831
+curl http://studies.thehyve.net/GSE27831/GSE27831_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE27831/GSE27831_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE27831/GSE27831_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE31773
+cd /var/www/datasets/GSE31773
+curl http://studies.thehyve.net/GSE31773/GSE31773_clinical.tar.xz -O
+
+mkdir /var/www/datasets/GSE32583
+cd /var/www/datasets/GSE32583
+curl http://studies.thehyve.net/GSE32583/GSE32583_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE32583/GSE32583_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE32583/GSE32583_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE32591
+cd /var/www/datasets/GSE32591
+curl http://studies.thehyve.net/GSE32591/GSE32591_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE32591/GSE32591_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE32591/GSE32591_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE34466
+cd /var/www/datasets/GSE34466
+curl http://studies.thehyve.net/GSE34466/GSE34466_clinical.tar.xz -O
+
+mkdir /var/www/datasets/GSE4271
+cd /var/www/datasets/GSE4271
+curl http://studies.thehyve.net/GSE4271/GSE4271_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE4271/GSE4271_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE4271/GSE4271_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE4698
+cd /var/www/datasets/GSE4698
+curl http://studies.thehyve.net/GSE4698/GSE4698_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE4698/GSE4698_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE4698/GSE4698_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE4922
+cd /var/www/datasets/GSE4922
+curl http://studies.thehyve.net/GSE4922/GSE4922_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE4922/GSE4922_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE4922/GSE4922_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE8581
+cd /var/www/datasets/GSE8581
+curl http://studies.thehyve.net/GSE8581/GSE8581_analysis.tar.xz -O
+curl http://studies.thehyve.net/GSE8581/GSE8581_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE8581/GSE8581_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE8581/GSE8581_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/GSE8650
+cd /var/www/datasets/GSE8650
+curl http://studies.thehyve.net/GSE8650/GSE8650_clinical.tar.xz -O
+curl http://studies.thehyve.net/GSE8650/GSE8650_expression.tar.xz -O
+curl http://studies.thehyve.net/GSE8650/GSE8650_ref_annotation.tar.xz -O
+
+mkdir /var/www/datasets/TCGAOV
+cd /var/www/datasets/TCGAOV
+curl http://studies.thehyve.net/TCGAOV/TCGAOV_acgh.tar.xz -O
+curl http://studies.thehyve.net/TCGAOV/TCGAOV_clinical.tar.xz -O
